6545ee42ae8497355912b99f92d0d028a6f00e9d
Lucas-G45 <lucas.gdcst@gmail.com>
2024-10-05 00:20:16 +0000

    Brinquei com variáveis na linguagem em C

--- ola.c
+++ ola.c
@@ -3,5 +3,22 @@
 
  void main () 
  {
-    printf("Olá mundo");
+    int x=0; //criando variável inteira "x"
+    printf("Valor de X: %d\n", x);
+    x++;
+    printf("Valor de X: %d\n", x);
+
+    float y = 12.345678; //criando variável real "y"
+    printf("Valor de y: %f\n", y); //Variável com todas as casas decimais
+    printf("Valor de y com 2 casas: %.2f\n", y); //Variável com apenas 2 casas decimais
+
+   char sexo = 'm'; // A variável do comando "char" sempre deve estar entre apóstrofos
+   printf("Sexo: %c\n", sexo);
+
+   char nome[30] = "Gabriel"; // Aqui foi criado um vetor com 30 espaços para caracteres
+   printf("Nome: %s\n", nome); // usa-se "%s" porque se trata de um vetor
+   printf("Primeira letra: %c\n", nome[0]);
+
+   printf("Olá mundo! \n");
+
  }
